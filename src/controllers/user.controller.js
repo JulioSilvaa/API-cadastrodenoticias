dc6ceaea48224680d3bdfc211f9f290a88798cac
Julio SIlva <juliocesar.amancio@yahoo.com.br>
2023-06-07 14:07:14 -0300

    refactor : changing user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,18 @@
-const user = (req, res) => {
-  res.send("JULIO SILVA");
+const create = (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  if (!name || !username || !password || !email) {
+    res.status(400).send({ message: "Favor preencher todos os campos" });
+  }
+
+  res.status(201).send({
+    message: "Usuário cadastrado com sucesso",
+    user: {
+      name,
+      username,
+      email,
+    },
+  });
 };
 
-module.exports = { user };
+module.exports = { create };
