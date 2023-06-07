58a3a4efc6526ec7223c61aa61a2ac703fc97bac
Julio SIlva <juliocesar.amancio@yahoo.com.br>
2023-06-07 12:26:26 -0300

    refactor: adding variable for server port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const useRoute = require("./src/routes/user.route");
 
+const port = 3000;
+
 app.use("/eu", useRoute);
 
-app.listen(8080);
+app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
