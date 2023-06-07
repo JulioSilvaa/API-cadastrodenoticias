3f21e03b2d3e72eebe73c3b27aeb134f68944323
Julio SIlva <juliocesar.amancio@yahoo.com.br>
2023-06-07 15:00:20 -0300

    refactor:  added dependencies and configurations

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
+const connectDatabase = require("./src/database/db");
 const useRoute = require("./src/routes/user.route");
+dotenv.config();
 
 const port = 3000;
+connectDatabase();
 
-app.use("/eu", useRoute);
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", useRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
