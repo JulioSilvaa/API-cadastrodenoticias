4ea32e264c0f207f38f9804130fd5eb623f435f9
Julio SIlva <juliocesar.amancio@yahoo.com.br>
2023-06-08 15:13:44 -0300

    feat : creating controls for news

---
+++ src/controllers/news.controller.js
@@ -0,0 +1,37 @@
+import {
+  createNewsService,
+  findAllNewsService,
+} from "../services/news.service.js";
+
+const createNews = async (req, res) => {
+  try {
+    const { title, text, banner } = req.body;
+
+    if (!title || !text || !banner) {
+      return res.sendStatus(400).send({ message: "Preencha todos os campos" });
+    }
+
+    await createNewsService({
+      title,
+      text,
+      banner,
+      user: { _id: "6480d341ea7805aa991a1839" },
+    });
+
+    res.send(201);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+const findAllNews = async (req, res) => {
+  try {
+    const news = await findAllNewsService();
+    if (news.length === 0) {
+      return res.status(400).send({ message: "não há noticias cadastradas" });
+    }
+    res.send(news);
+  } catch (error) {}
+};
+
+export { createNews, findAllNews };
